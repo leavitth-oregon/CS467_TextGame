a6c97ac4093b7edc0acca4ef996f3fec11fb244c
leavitth-oregon <82043841+leavitth-oregon@users.noreply.github.com>
2023-02-11 17:57:13 -0500

    Create fugue.py

---
+++ fugue.py
@@ -0,0 +1,25 @@
+# This is the main executable file for Fugue that will control all other files associated with the game 
+# Luke Babcock, John Koenig, and Hayley Leavitt 2023
+
+def opening():
+    return
+
+
+def main_menu():
+    return
+
+
+def main():
+    # print the game opening
+    opening()
+    
+    # print the main menu
+    main_menu()
+    
+    # once the user selects play game, start the game
+    fugue()
+    return
+
+if __name__ == "__main__":
+    main()
+
