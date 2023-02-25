58b594c0b474ef460518753b35fb7c6e7dee1894
leavitth-oregon <82043841+leavitth-oregon@users.noreply.github.com>
2023-02-25 01:03:45 -0500

    Update fugue_jukebox.py
    Added Windows functionality - needs testing

--- fugue_jukebox.py
+++ fugue_jukebox.py
@@ -7,15 +7,18 @@
 # https://docs.python.org/3/library/threading.html 
 # https://python.plainenglish.io/play-music-using-python-subprocess-8c7a1bde2271 
 # https://www.bogotobogo.com/python/python_subprocess_module.php 
+# https://stackoverflow.com/questions/307305/play-a-sound-with-python
 
 # libraries
 import subprocess 
 from threading import Thread
 import time
+import winsound
 
 # global variables
 mediaplayer = ["afplay ", "mpg123 "]
 song_files = ["fugue_combat.mp3", "fugue_credits.mp3", "fugue_ending.mp3", "fugue_memories.mp3", "fugue_paths_and_roads.mp3", "fugue_throne_room.mp3", "fugue_tower_bedroom.mp3"]
+operating_system = "MacOS"
 
 # classes 
 class Jukebox(): 
@@ -25,20 +28,32 @@ class Jukebox():
         self._p = None
 
     def play_song(self, song_file) -> None:
-        # create the player process
-        self._p = subprocess.Popen([self.musicplayer,    # the music player, in this case mpg123
-                                '-C',                  # Enable commands to be read from stdin
-                                '-q',                  # Use quiet mode, which has less terminal alerts
-                                song_file],            # the song we want to play
-                                stdin = subprocess.PIPE, 
-                                stderr = None) 
+        if operating_system != "Windows":
+            # create the player process 
+            self._p = subprocess.Popen([self.musicplayer,     # the music player, in this case, mpg123
+                                    '-C',                     # Enable commands to be read from stdin
+                                    '-q',                     # Use quiet mode, which has less terminal alerts
+                                    song_file],               # the song we want to play
+                                    stdin = subprocess.PIPE,  # pipe the subprocess
+                                    stderr = None)            # no error
 
-        # stop mpg123 from complainging when we exit
-        self._p.stdin.write(b'')
-        self._p.stdin.flush()
+            # handle exit procedures of the subprocess
+            self._p.stdin.write(b'')
+            self._p.stdin.flush()
+        
+        else: 
+            self._p = subprocess.Popen(winsound.PlaySound(song_file, winsound.SND_FILENAME),   # the music player, in this case, winsound
+                                       stdin = subprocess.PIPE,                                # pipe the subprocess
+                                       stderr = None)                                          # no error
+
+            # handle exit procedures of the subprocress
+            self._p.stdin.write(b'')
+            self._p.stdin.flush()
+
         return
 
     def stop_song(self) -> None: 
+        # handle exit procedures
         self._p.stdin.write(b'q')
         self._p.stdin.flush()
 
