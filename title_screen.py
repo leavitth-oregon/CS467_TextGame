f537bc6ad92587deed6c673d52267f288f7f4ace
babcockl <122835213+babcockl@users.noreply.github.com>
2023-03-12 16:53:10 -0400

    Update title_screen.py
    Added credits

--- title_screen.py
+++ title_screen.py
@@ -16,6 +16,9 @@ def title_screen():
 
     choice = input("What would you like to do?: \n")
 
+# Choices:
+    
+ 
     if choice == "1" or choice == "new game".lower():
         os.system('cls')
         new_room = fugue_map.campfire
@@ -27,3 +30,34 @@ def title_screen():
         print("###############################################################################################")
 
         return
+    
+    
+#
+    elif choice == "2" or choice.lower == "continue":
+        #  Load Game
+        pass
+
+
+#
+    elif choice == "3" or choice.lower() == "credits":
+        os.system('cls')
+        print("###############################################################################################")
+        print("                                        FUGUE                                                 ")
+        print("                                         BY                                                   \n")
+        print("                                    Hayley Leavitt                                            ")
+        print("                                     John Koenig                                              ")
+        print("                                     Luke Babcock                                             \n")
+        print("###############################################################################################\n")
+        input("Press any key to go back")
+        title_screen()
+
+
+#
+    elif choice == "4" or choice.lower() == "exit":
+        sys.exit("Thank you for playing!")
+
+
+#
+    else:
+        print("Invalid selection. Try again: ")
+
