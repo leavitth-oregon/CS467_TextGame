7bbf8944b41fd67dfc87630889293ca4c50d4232
babcockl <122835213+babcockl@users.noreply.github.com>
2023-03-12 09:44:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import title_screen
 import inventory
 import commands
 import fugue_map
+import os
 
 
 my_inventory = inventory.Inventory()
 current_room = fugue_map.campfire
-
+os.system('cls')
 title_screen.title_screen()
 commands.player_prompt()
