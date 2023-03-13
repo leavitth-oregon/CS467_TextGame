63eb8adb5fd020f8fc469c0fbd99cac934ac721b
babcockl <122835213+babcockl@users.noreply.github.com>
2023-03-13 15:42:40 -0400

    Update main.py
    Updated main

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import commands
 import fugue_map
 import os
 
+my_map = fugue_map.Fugue_Map()
+my_map.prep_data()
+
 
 my_inventory = inventory.Inventory()
-current_room = fugue_map.campfire
+current_room = my_map.desert_camp
 os.system('cls')
 title_screen.title_screen()
+print(current_room.items)
 commands.player_prompt()
